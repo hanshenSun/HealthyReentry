7635f1d4a4d47aad0cb774637cdfaadb036cb35d
amitlzkpa <amit.lzkpa@gmail.com>
2020-06-04 12:45:29 -0500

    use date argument for calculating graphs; count backward from user's last status update

--- server/routes/api/admin.js
+++ server/routes/api/admin.js
@@ -145,7 +145,11 @@ router.post("/graph", async function(req, res) {
   }
   let graphs = [];
   for(let email of emailList) {
-    let graph = await eg(email, incubationDays); // TODO set each user's latest status date
+    let user = await User.findOne({"email": email});
+    let status = await Status.find({ "user": user._id })
+                             .sort({ date: -1 })
+                             .limit(1);
+    let graph = await eg(email, incubationDays, status[0].date);
     graphs.push(graph);
   }
   res.json(graphs);
