1eb9091f20fd91d009bd3ac60710a09667ae1a40
amitlzkpa <amit.lzkpa@gmail.com>
2020-06-23 09:39:13 -0500

    use ssl cert for front end webpack server

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const fs = require('fs');
 
 const PORT = process.env.PORT || 8080;
 
@@ -7,9 +8,15 @@ module.exports = {
     progress: false,
     proxy: {
       "/api/": {
-        target: `http://localhost:${PORT}/`,
+        target: `https://localhost:${PORT}/`,
         logLevel: "debug"
       }
+    },
+    http2: true,
+    https: {
+      key: fs.readFileSync('./testingCertificates/ttlocalcert.key'),
+      cert: fs.readFileSync('./testingCertificates/ttlocalcert.crt'),
+      ca: fs.readFileSync('./testingCertificates/ttlocalcert.crt')
     }
   },
   lintOnSave: false,
